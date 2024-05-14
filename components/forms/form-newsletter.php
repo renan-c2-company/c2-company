5e164790d96ad4f0ce46dedb538a8fbc1e2fe203
renandevcwb <renandevcwb@gmail.com>
2024-05-14 10:45:08 -0300

    Adiciona newsletter e pastas necessárias

---
+++ components/forms/form-newsletter.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * 
+ * Formulário de Newsletter
+ * 
+ * @package c2-company
+ * @since 1.0.0
+ * 
+ */?>
+<!-- Formulário de Inscrição em Newsletter -->
+<div class="c2-newsletter">
+    <form action="" method="get" class="form-group">
+        <input class="form-control" type="email" placeholder="Digite o seu e-mail"/>
+    </form>
+</div>
+<!-- Fim de Formulário de Inscrição em Newsletter -->
--- parts/header.php
+++ parts/header.php
@@ -7,3 +7,11 @@
  * @since 1.0.0
  * 
  */
+if( !is_front_page() || !is_page() ):?>
+<!-- Cabeçalho da Página -->
+<header class="container-fluid" role="banner">
+    
+</header>
+<!-- Fim do cabeçalho da Página -->
+<?php
+endif;
